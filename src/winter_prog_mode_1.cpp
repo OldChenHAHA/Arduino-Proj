dc5b4d62b3c659e65a68ae8e28ce568534e8bcca
OldChen <chen_zhongyao@163.com>
2017-05-14 18:10:30 +0800

    Update winter_prog_mode_1.cpp

--- src/winter_prog_mode_1.cpp
+++ src/winter_prog_mode_1.cpp
@@ -1,6 +1,8 @@
-#define NOT_CLOSED HIGH
-#define HAVE_INPUT HIGH
-
+#define NOT_CLOSED HIGH //没闭合
+#define HAVE_INPUT HIGH  // 有输入
+#define START_UP HIGH // 启动
+#define SHUT_DOWN LOW // 停止
+#define CUT_OFF LOW // 断开
 
 
 
@@ -14,19 +16,19 @@ int winter_prog_mode_1()
       return 1;
     }
 
-    int val_PIN_11 = digitalRead(PIN_11);
-    int val_PIN_14 = digitalRead(PIN_14);
-    int val_PIN_21 = digitalRead(PIN_21);
-    int val_PIN_22 = digitalRead(PIN_22);
-    int val_PIN_23 = digitalRead(PIN_23);
-    int val_PIN_24 = digitalRead(PIN_24);
-    int val_PIN_25 = digitalRead(PIN_25);
-    int val_PIN_26 = digitalRead(PIN_26);
-    int val_PIN_27 = digitalRead(PIN_27);
-    int val_PIN_30 = digitalRead(PIN_30);
-    int val_PIN_31 = digitalRead(PIN_31);
-    int val_PIN_32 = digitalRead(PIN_32);
-    int val_PIN_33 = digitalRead(PIN_33);
+    int val_PIN_i11 = digitalRead(PIN_i11);
+    int val_PIN_i14 = digitalRead(PIN_i14);
+    int val_PIN_i21 = digitalRead(PIN_i21);
+    int val_PIN_i22 = digitalRead(PIN_i22);
+    int val_PIN_i23 = digitalRead(PIN_i23);
+    int val_PIN_i24 = digitalRead(PIN_i24);
+    int val_PIN_i25 = digitalRead(PIN_i25);
+    int val_PIN_i26 = digitalRead(PIN_i26);
+    int val_PIN_i27 = digitalRead(PIN_i27);
+    int val_PIN_i30 = digitalRead(PIN_i30);
+    int val_PIN_i31 = digitalRead(PIN_i31);
+    int val_PIN_i32 = digitalRead(PIN_i32);
+    int val_PIN_i33 = digitalRead(PIN_i33);
 
     if (val_PIN_11==HAVE_INPUT || val_PIN_14==HAVE_INPUT || val_PIN_21==HAVE_INPUT || val_PIN_22==HAVE_INPUT || val_PIN_23==HAVE_INPUT || val_PIN_24==HAVE_INPUT || val_PIN_25==HAVE_INPUT || val_PIN_26==HAVE_INPUT || val_PIN_27==HAVE_INPUT || val_PIN_30==HAVE_INPUT || || val_PIN_31==HAVE_INPUT || || val_PIN_32==HAVE_INPUT || || val_PIN_33==HAVE_INPUT )
     {
@@ -35,21 +37,125 @@ int winter_prog_mode_1()
       return 0;
     }
 
-    if ( digitalRead(PIN_34) == CLOSED ) // 检测3.4是否闭合
+    if ( digitalRead(PIN_i34) == CLOSED ) // 检测i3.4是否闭合
     {
-        // 是
+        // i3.4闭合
         if ( digitalRead(PIN_06) == CLOSED )
         {
+            delay(3000); // T06延时3秒，启动Q1.7
+            digitalWrite(PIN_Q17, START_UP);
+            if ( digitalRead(PIN_22) == CLOSED )
+            {   //是
+                digitalWrite(PIN_Q17, SHUT_DOWN); // 停Q1.7
+                save_alert_msg("2.2闭合");  // 存储报警信息
+                Serial485.printf("2.2闭合"); // 485输出
+                return 0;
+            }
+            else 
+            {
+                goto tag1;
+            }
 
         }
-        else
+        else // 0.6没闭合
         {
+            delay(3000); // T06延时3s，启动Q2.0
+            digitalWrite(PIN_Q20, START_UP);
+            if (digitalRead(PIN_26) == CLOSED) // 检测2.6是否闭合
+            { //2.6闭合
+                /* code */
+            }
+            else //2.6没闭合
+            {
+                tag1:if( digitalRead(PIN_i10) ) // i1.0是否闭合
+                { // i1.0 闭合
+                    delay(3000); //T08延时3s，启动Q2.3
+                    digitalWrite(PIN_Q23, START_UP);
+                    if (digitalRead(PIN_i24) == CLOSED)//检测i2.4是否闭合
+                    {//i2.4闭合
+                        //停Q2.3，报警
+                        digitalWrite(PIN_Q23, SHUT_DOWN);
+                        save_alert_msg("i2.4闭合");  // 存储报警信息
+                        Serial485.printf("i2.4闭合"); // 485输出
+                        return 0;
+                    }
+                    else
+                    {//i2.4没闭合
+                        goto tag2;
+                    }
+                }
+                else
+                {// i1.0 没闭合
+                    //T08延时3s，启动Q2.4
+                    delay(3000);
+                    digitalWrite(PIN_Q24, START_UP);
+                    if ( digitalRead(PIN_i30)==CLOSED )//检测i3.0是否闭合
+                    {//i3.0 闭合
+                        // 停Q2.4 报警
+                        digitalWrite(PIN_Q24, SHUT_DOWN);
+                        save_alert_msg("i3.0闭合");  // 存储报警信息
+                        Serial485.printf("i3.0闭合"); // 485输出
+                        return 0;
+                    }
+                    else
+                    {// i3.0没闭合
+                        tag2: if (digital(PIN_34) == CUT_OFF) // 检测3.4是否断开
+                        {// 3.4断开
+                            if( digitalRead(PIN_i20) == CUT_OFF )//i2.0是否断开
+                            {// i2.0断开
+                                // 停Q1.7或者Q2.0 存信息
+                                digitalWrite(PIN_Q17, SHUT_DOWN);
+                                save_alert_msg("i2.0断开");  // 存储报警信息
+                                Serial485.printf("i2.0断开"); // 485输出
+                                return 0;
+                            }
+                            else
+                            {// i2.0 没断开
+                                goto tag3;
+                            }
+                        }
+                        else
+                        {//3.4没断开
+                            goto tag3;
+                        }
+                    }
+
+                }
+
+            }
+
 
         }
     }
     else
-    {
-        // 否
+    {// i3.4没闭合
+        tag3:if( digitalRead(PIN_i01)==CLOSED )//i0.1是否闭合
+        {// i0.1闭合
+
+
+        }
+        else
+        {// i0.1没闭合
+            if()//i1.7是否闭合
+            {// i1.7闭合
+
+
+
+            }
+            else
+            {// i1.7没闭合
+                return 0;
+            }
+
+        }
+
+
+
+
+
+
+
+        
     }
 
 
